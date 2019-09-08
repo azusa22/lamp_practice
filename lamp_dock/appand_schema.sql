329ca03abcebc8c3f294d6eab3cf6d2e5f1366f5
azusa22 <azuuuu.i.23@gmail.com>
2019-09-08 21:48:14 +0900

    #6 追加テーブル設計

--- lamp_dock/appand_schema.sql
+++ lamp_dock/appand_schema.sql
@@ -1,7 +1,6 @@
 create table order_historys (
   order_number int auto_increment,
   user_id int,
-  cart_id int,
   date datetime,
   primary key(order_number)
 );
@@ -9,7 +8,5 @@ create table order_historys (
 create table order_details (
   order_number int,
   item_id int,
-  name varchar(100),
-  price int,
   amount int
 );
